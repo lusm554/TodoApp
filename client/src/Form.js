3b2a596565bf14b6e80c258bda14e9acb2e24dc9
loveyousomuch <loveyousomuch554@gmail.com>
2020-07-26 00:51:28 +0300

    👺

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
 
-class Input extends Component {
+
+class List extends Component {
     constructor(props) {
         super(props)
-        this.state = {list: []}
+        this.state = {list: [], value: ''}
+    }
+
+    hadleChange(e) {
+        this.setState({value: e.target.value})
+    }
+
+    hadleClick(e) {
+        this.setState(state => ({
+            list:   [...state.list, state.value]
+        }))
+
+        e.preventDefault()
     }
 
     render() {
-        return <h1>Hello,World!</h1>
+        return (
+            <div>
+                <form>
+                    <label>
+                        Task:<br/>
+                        <input 
+                        type="text" 
+                        value={this.state.value} 
+                        onChange={this.hadleChange.bind(this)} 
+                        placeholder='hui'
+                        />
+                    </label>
+
+                    <input 
+                    type='submit' 
+                    onClick={this.hadleClick.bind(this)} 
+                    value='Add to list'
+                    />
+                </form>
+                <TodoList toDoList={this.state.list}/>
+            </div>
+        )
     }
 }
 
-export default Input;
+class TodoList extends Component {
+    render() {
+        return (
+            <ul>
+                {
+                    this.props.toDoList.map((item, i)=> {
+                        return <li key={i+1} >{item}</li>
+                    })
+                }
+            </ul>
+        )
+    }
+}
+
+export default List;
