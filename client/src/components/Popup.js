436e0ff9d14723b488dc475c0573bf9eb48c32ce
loveyousomuch <loveyousomuch554@gmail.com>
2020-07-28 04:03:21 +0300

    change method in react 🥴

---
+++ client/src/components/Popup.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './popup.css';
+
+class Popup extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {changedTitle: '', changedTask: ''}
+    }
+
+    render() {
+        const title = this.state.changedTitle, task = this.state.changedTask;
+
+        return (
+            <div className='popup'>
+                <div className='popup_inner'>
+                    <form>
+                        <label>
+                            Change task:<br/>
+                            <input 
+                              placeholder='title...'
+                              onChange={e => this.setState({changedTitle: e.target.value})}
+                            /><br/>
+                            <textarea 
+                              placeholder='task...'
+                              onChange={e => this.setState({changedTask: e.target.value})}
+                            />
+                        </label>
+                    </form>
+                    <button onClick={() => this.props.closePopup({title, task})}>Close window</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Popup;
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import Popup from './Popup';
 
 class TaskList extends Component {
     constructor(props) {
         super(props)
-        this.state = {tasks: []}
+        this.state = {tasks: [], showPopup: false}
 
         this.handleToggleChange = this.handleToggleChange.bind(this)
         this.getTasks = this.getTasks.bind(this)
         this.handleClickDelete = this.handleClickDelete.bind(this)
+        this.togglePopup = this.togglePopup.bind(this)
     }
 
     componentDidMount() {
@@ -40,6 +42,24 @@ class TaskList extends Component {
             .then(this.getTasks)
     }
 
+    handleChangeTask({_id}, {title, task}) {
+        fetch(`/api/change/${_id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({title, task})
+        })
+            .then(res => {
+                console.log(res);
+                this.setState(({showPopup}) => ({showPopup: !showPopup}))
+            })
+    }
+
+    togglePopup() {
+        this.setState(({showPopup}) => ({showPopup: !showPopup}))
+    }
+
     render() {
         const { tasks } = this.state
 
@@ -59,6 +79,13 @@ class TaskList extends Component {
                           value="delete"
                           onClick={() => this.handleClickDelete({_id})}
                         />
+                        <input 
+                          type="button"
+                          value="change"
+                          onClick={this.togglePopup}
+                        />
+                        {this.state.showPopup && 
+                          <Popup text="close" closePopup={this.handleChangeTask.bind(this, {_id})}/> }
                     </li>
                 })}
             </ul>
